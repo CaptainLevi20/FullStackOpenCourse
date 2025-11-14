f7d240bda650296d6a09833fb4652b868b38fa0b
Andres Ulloa <asantiagoulloa@gmail.com>
2025-11-14 18:54:42 -0500

    Part 2: Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,48 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "040-123456", id: 1 },
-		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
-		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-	]);
-	const [newName, setNewName] = useState("");
-	const [newNumber, setNewNumber] = useState("");
-	const [search, setSearch] = useState("");
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
-	function submitForm(e) {
-		e.preventDefault();
-		if (!persons.some((person) => person.name === newName)) {
-			setPersons((prevState) => {
-				return [...prevState, { name: newName, number: newNumber }];
-			});
-			setNewName("");
-			setNewNumber("");
-		} else {
-			alert(`${newName} is already added to Phonebook`);
-		}
-	}
+  function submitForm(e) {
+    e.preventDefault();
+    if (!persons.some((person) => person.name === newName)) {
+      setPersons((prevState) => {
+        return [...prevState, { name: newName, number: newNumber }];
+      });
+      setNewName("");
+      setNewNumber("");
+    } else {
+      alert(`${newName} is already added to Phonebook`);
+    }
+  }
 
-	return (
-		<div>
-			<h2>Phonebook</h2>
-			<Filter search={search} persons={persons} setSearch={setSearch} />
-			<h3>Add new</h3>
-			<PersonForm
-				newName={newName}
-				newNumber={newNumber}
-				submitForm={submitForm}
-				setNewName={setNewName}
-				setNewNumber={setNewNumber}
-			/>
-			<h3>Numbers</h3>
-			<Persons persons={persons} />
-		</div>
-	);
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter search={search} persons={persons} setSearch={setSearch} />
+      <h3>Add new</h3>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        submitForm={submitForm}
+        setNewName={setNewName}
+        setNewNumber={setNewNumber}
+      />
+      <h3>Numbers</h3>
+      <Persons persons={persons} />
+    </div>
+  );
 };
 
 export default App;
