ddfcd39350aaea1069d4c64bc9bb455350208d57
Andres Ulloa <asantiagoulloa@gmail.com>
2026-01-25 20:00:17 -0500

    Part 3: Exercise 3.13 - 3.14

--- part3/server.js
+++ part3/server.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
+const Person = require("./models/person");
 
 app.use(cors());
 app.use(express.json());
@@ -11,44 +13,19 @@ morgan.token("post", (req) => {
 });
 
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :post"),
+  morgan(":method :url :status :res[content-length] - :response-time ms :post")
 );
 
 app.use(express.static("dist"));
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 const generateID = () => {
   return Math.floor(Math.random() * 10000);
 };
 
-const isUniqueName = (name) => {
-  return persons.find((person) => person.name === name) === undefined;
-};
-
 app.get("/api/persons", (request, response) => {
-  response.send(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.post("/api/persons", (request, response) => {
@@ -64,19 +41,14 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  if (isUniqueName(request.body.name)) {
-    const person = {
-      id: generateID(),
-      name: request.body.name,
-      number: request.body.number,
-    };
-    persons = persons.concat(person);
-    response.json(person);
-  } else {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
+  const person = new Person({
+    id: generateID(),
+    name: request.body.name,
+    number: request.body.number,
+  });
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
@@ -98,7 +70,7 @@ app.delete("/api/persons/:id", (request, response) => {
 app.get("/info", (request, response) => {
   response.send(
     `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>`,
+    <p>${new Date()}</p>`
   );
 });
 
