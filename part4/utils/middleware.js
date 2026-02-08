3c7ce7ffd090e8cc420b451c8c2539e4dc35c0da
Andres Ulloa <asantiagoulloa@gmail.com>
2026-02-08 10:29:12 -0500

    Part 4: Exercises 4.15 - 4.20

--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,24 +1,36 @@
-const logger = require('./logger')
+const logger = require("./logger");
 
 const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method)
-  logger.info('Path:  ', request.path)
-  logger.info('Body:  ', request.body)
-  logger.info('---')
-  next()
-}
+  logger.info("Method:", request.method);
+  logger.info("Path:  ", request.path);
+  logger.info("Body:  ", request.body);
+  logger.info("---");
+  next();
+};
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
+  if (error.name === 'MongoServerError' && error.code === 11000) {
+    return response.status(400).json({
+      error: 'username must be unique'
+    })
+  }
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({
+      error: error.message
+    })
+  }
+
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+    return response.status(400).send({
+      error: 'malformatted id'
+    })
   }
 
   next(error)
@@ -28,4 +40,4 @@ module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
-}
+};
