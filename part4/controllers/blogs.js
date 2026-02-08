081a4c27d8edc5c216a598db493d53858c7d29e0
Andres Ulloa <asantiagoulloa@gmail.com>
2026-02-08 09:01:53 -0500

    Part 4 : Exercises 4.13 - 4.14

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,23 +1,39 @@
-const blogsRouter = require("express").Router();
-const Blog = require("../models/blog");
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
 
-blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
-  response.json(blogs);
-});
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
 
-blogsRouter.post("/", async (request, response) => {
-  if (!request.body.title || !request.body.url) {
-    return response.status(400).json({ error: "title or url is missing" });
+blogsRouter.post('/', async (request, response) => {
+  if (!request.body.title || !request.body.url){
+    return response.status(400).json({ error: 'title or url is missing' })
   }
   const blog = new Blog({
     title: request.body.title,
     author: request.body.author,
     url: request.body.url,
-    likes: request.body.likes ?? 0,
-  });
-  const result = await blog.save();
-  response.status(201).json(result);
-});
+    likes: request.body.likes ?? 0
+  })
+  const result = await blog.save()
+  response.status(201).json(result)
+})
 
-module.exports = blogsRouter;
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    { title, author, url, likes },
+    { new: true }
+  )
+  response.json(updatedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+module.exports = blogsRouter
